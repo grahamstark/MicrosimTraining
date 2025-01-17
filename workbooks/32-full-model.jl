e73d95e037542940b991f2e03cff72efd806425f
grahamstark <graham.stark@virtual-worlds.biz>
2025-01-17 23:13:08 +0000

    full proto

--- workbooks/32-full-model.jl
+++ workbooks/32-full-model.jl
@@ -57,7 +57,7 @@ begin
 		allowance::Number, 
 		income_tax_rate::Number, 
 		uc_taper::Number, 
-		settings::Settings )::NamedTuple
+		settings::Settings )::Tuple
 		global running_total
 		@show allowance 
 		@show income_tax_rate
@@ -73,7 +73,7 @@ begin
 		running_total = 0
 		results = Runner.do_one_run( settings, sys, run_progress )
 		summary = summarise_frames!( results, settings )
-		summary
+		summary, results
 	end
 
     PlutoUI.TableOfContents(aside=true)
@@ -81,46 +81,23 @@ end
 
 # ╔═╡ 3a2a55d5-8cf8-4d5c-80a7-84a03923bba8
 md"""
-## A Budget for ...
+# A War On Poverty
 """
-
-# ╔═╡ 15c504c8-4a72-4aa5-b83f-4d03c3659df9
-begin
-    md"""
-    Finally in this section, let's consider taxes and benefits together.
-    """
-end 
 
 # ╔═╡ 5c5b2176-148b-4f5c-a02c-5e9e82df11c3
 begin
     md"""
-    We have the usual diagram, but now all the fields for taxes and benefits together on the left. So it's possible to design a complete system.
+The Government of Unicoria has plegded to reduce the headcount measure of poverty by 3 percentage points. Your task is to design a policy package that delivers this in the most effective way possible. The economy is doing well, and net extra spending of up to £4 billion has been agreed.
+* *cost* any costs above £4bn will have to be raised from somewhere;
+* *targetting vs. incentives*: well targetted benefit increases may force poor people into poverty traps, whilst more widely spread increases may require tax increases to keep within budget; 
+* *alternative measures of poverty* have you cheated at all, and reduced poverty headcounts by concentrating on the near-poor, perhaps through increasing in-work benefits? (You may need to do this in order to meet the political objectives)
     """
 end 
-
-# ╔═╡ b267f167-6f9b-49e3-9d6e-ac9c449ae180
-begin
-    md"""
-    We've seen seen that you can understand the effects of taxes and benefits using the same concepts. Now, when we put the two sides together, we can analyse the results in the same way, but the tax side and the benefit side can interact in ways that produce complicated and often unintended outcomes.
-    """
-end 
-
-# ╔═╡ c5f6f64e-7a1c-4fc3-836d-aafde14b44d8
-md"""
-    For example, if tax allowances are low and the generosity of in-work benefits fairly high, it's possible to pay taxes and receive means-tested benefits at the same time. Depending on the exact calculations involved, this can produce METRs in excess of 100%  - earn more and net income falls.
-    """
-
-
-# ╔═╡ 99152830-e5b1-4541-8b0a-ad51e1168f95
-aside( md"See, for example: [Stark and Dilnot](^FN_STARK_DILNOT-2)"; v_offset=-80 )
-
-# ╔═╡ 3ed3fa2f-2c1b-4915-9f39-b5603e62e2c3
-
 
 # ╔═╡ d447f5dd-253c-4c8a-a2d4-873d50c9a9ec
 begin
     md"""
-    ##### Activity
+    ##### Taxes and Benefits
     """
 end 
 
@@ -135,25 +112,13 @@ benefit withdrawal rate: $(@bind uc_taper NumberField(0:1:100,default=55) )) (%)
 """
 end
 
-# ╔═╡ 0a2e393c-9fbb-4bbb-a6af-96aeac5cb458
-# ╠═╡ show_logs = false
-begin
-sys2 = deepcopy(ANNUAL_BASE_SYS)
-		itdiff = sys2.it.non_savings_rates[2] - income_tax_rate
-		println( "itdiff=$itdiff")
-		sys2.it.non_savings_rates .-= itdiff
-		
-println( "after: sys2.it.non_savings_rates $(sys2.it.non_savings_rates)")
-end
-
 # ╔═╡ 627959cf-6a7c-4f87-82f7-406f5c7eb76a
 # ╠═╡ show_logs = false
-summary= run_model( tax_allowance, income_tax_rate, uc_taper, settings );
-
+summary, data = run_model( tax_allowance, income_tax_rate, uc_taper, settings );
 
 # ╔═╡ d2188dd8-1240-4fdd-870b-dcd15e91f4f2
 begin
-	function draw_summary_graphs( summary :: NamedTuple )::Figure
+	function draw_summary_graphs( summary :: NamedTuple, data::NamedTuple )::Figure
 		f = Figure()
 		ax1 = Axis(f[1,1]; title="Lorenz Curve", xlabel="Population Share", ylabel="Income Share")
 		popshare = summary.quantiles[1][:,1]
@@ -162,13 +127,18 @@ begin
 		insert!(popshare,1,0)
 		insert!(incshare_pre,1,0)
 		insert!(incshare_post,1,0)
-		lines!(ax1, popshare, incshare_pre; label="Before")
-		lines!(ax1,popshare,incshare_post; label="After")
+		lines!(ax1, popshare, incshare_pre; label="Before", color=(:lightsteelblue, 1))
+		lines!(ax1,popshare,incshare_post; label="After", color=(:gold2, 1))
 		lines!(ax1,[0,1],[0,1]; color=:grey)
 		ax2 = Axis(f[1,2]; title="Income Changes By Decile", 
 			ylabel="Change in £s per week", xlabel="Decile" )
 		dch = summary.deciles[2][:, 3] .- summary.deciles[1][:, 3]
 		barplot!( ax2, dch)
+		ax3 = Axis(f[2,1:2]; title="Income Distribution", xlabel="£s pw", ylabel="")
+		density!( ax3, data.indiv[1].eq_bhc_net_income; 
+			weights=data.indiv[1].weight, label="Before", color=(:lightsteelblue, 0.5))
+		density!( ax3, data.indiv[2].eq_bhc_net_income; 
+			weights=data.indiv[2].weight, label="After", color=(:gold2, 0.5) )
 		f
 	end
 
@@ -217,10 +187,8 @@ begin
 		losers=fc( summary.gain_lose[2].losers ),
 		nc = fc( summary.gain_lose[2].nc) )
 	end
-
-		
-
-	draw_summary_graphs( summary )
+	
+	draw_summary_graphs( summary, data )
 end
 
 # ╔═╡ d069cd4d-7afc-429f-a8fd-3f1c0a640117
@@ -244,69 +212,20 @@ Households gaining: **$(res.gainers)** losing: **$(res.losers)** unchanged: **$(
 """
 end
 
-# ╔═╡ 8c34657d-e843-4ff2-9c01-bdadc98c0a0e
-begin
-    md"""
-    Exercise: we simply invite you to experiment here. Try to get  a feel for how taxes and benefits interact.
-    """
-end 
+# ╔═╡ 8cff2a32-35b5-4330-8bfe-0dc604438dba
+hint(md"Maybe try changing the witghdrawal rate.")
 
-# ╔═╡ 154ed134-8431-4792-a915-9ffcadf0016e
-begin
-    md"""
-
-#### Integrating Taxes and Benefits
-	
-The sometimes chaotic nature of the interaction of taxes and benefits have led to advocacy of the complete integration of taxes and benefits. Completely integrated systems are sometimes known as *Negative Income Taxes*; one such system was proposed for the UK in the early 1970s[^FN_SLOMAN]. Universal Credit, briefly discussed above, is an attempt to integrate several means-tested benefits, though not taxes. However, though it produces messy and inconsistent interactions, there are often good reasons for keeping parts of the tax and benefit system separate. With Minimum Income benefits, for example, it's often important to get help to people very quickly if they are destitute, whereas with an in-work benefit, it's often helpful to take a longer view, so support can be given consistently over say a, year.
-    """
-end 
-
-# ╔═╡ a367666b-f8eb-425e-9ccd-ff54f8e5626f
-begin
-    md"""
-    #### Replacement Rates
-    """
-end 
-
-# ╔═╡ b1033397-d349-4aba-855f-e500102c3e6b
-begin
-    md"""
-    Now we have taxes and benefits together, we can introduce our fourth summary measure: the *replacement rate*. This is intended to be a measure of whether it is worth working at all, and simply the ratio between the net income someone would have when working some standard amount of hours (usually 40 per week), and the income received when not working at all.
-    """
-end 
-
-# ╔═╡ 04eb3fe6-7279-46d5-b9b2-2770858c9f0b
-begin
-    md"""
-    ![Illustration of the replacement rate](./images/bc-2.png)
-    """
-end 
-
-# ╔═╡ de3ef31d-f22a-414b-bf0a-c4d516d82cc2
-begin
-    md"""
-    The replacement rate is worth knowing about because it is sometimes used in academic studies trying to explain, for example, the aggregate level of unemployment; it also sometimes appears in popular discourse[^FN_RP].
-    """
-end 
+# ╔═╡ 67813f80-f4bd-41b3-8963-937e015614c5
+danger(md"Don't forget to commit your saved notebook to your repository.")
 
 # ╔═╡ Cell order:
 # ╟─3a2a55d5-8cf8-4d5c-80a7-84a03923bba8
 # ╟─72c7843c-3698-4045-9c83-2ad391097ad8
-# ╟─0a2e393c-9fbb-4bbb-a6af-96aeac5cb458
-# ╟─15c504c8-4a72-4aa5-b83f-4d03c3659df9
 # ╟─5c5b2176-148b-4f5c-a02c-5e9e82df11c3
-# ╟─b267f167-6f9b-49e3-9d6e-ac9c449ae180
-# ╟─c5f6f64e-7a1c-4fc3-836d-aafde14b44d8
-# ╟─99152830-e5b1-4541-8b0a-ad51e1168f95
 # ╟─d069cd4d-7afc-429f-a8fd-3f1c0a640117
 # ╟─d2188dd8-1240-4fdd-870b-dcd15e91f4f2
-# ╠═627959cf-6a7c-4f87-82f7-406f5c7eb76a
-# ╠═3ed3fa2f-2c1b-4915-9f39-b5603e62e2c3
+# ╟─627959cf-6a7c-4f87-82f7-406f5c7eb76a
 # ╟─d447f5dd-253c-4c8a-a2d4-873d50c9a9ec
 # ╟─4aa314f2-3415-4482-a042-d4c7ebd1cb21
-# ╟─8c34657d-e843-4ff2-9c01-bdadc98c0a0e
-# ╟─154ed134-8431-4792-a915-9ffcadf0016e
-# ╟─a367666b-f8eb-425e-9ccd-ff54f8e5626f
-# ╟─b1033397-d349-4aba-855f-e500102c3e6b
-# ╟─04eb3fe6-7279-46d5-b9b2-2770858c9f0b
-# ╟─de3ef31d-f22a-414b-bf0a-c4d516d82cc2
+# ╟─8cff2a32-35b5-4330-8bfe-0dc604438dba
+# ╟─67813f80-f4bd-41b3-8963-937e015614c5
