c879f0819b43e9d0f82f18071e7aec924d362210
grahamstark <graham.stark@virtual-worlds.biz>
2025-01-18 11:33:05 +0000

    povline snafu

--- workbooks/32-full-model.jl
+++ workbooks/32-full-model.jl
@@ -87,28 +87,22 @@ md"""
 # ╔═╡ 5c5b2176-148b-4f5c-a02c-5e9e82df11c3
 begin
     md"""
-The Government of Unicoria has plegded to reduce the headcount measure of poverty by 3 percentage points. Your task is to design a policy package that delivers this in the most effective way possible. The economy is doing well, and net extra spending of up to £4 billion has been agreed.
+The Government of Unicoria has pledged to reduce the headcount measure of poverty by 3 percentage points. Your task is to design a policy package that delivers this in the most effective way possible. The economy is doing well, and net extra spending of up to £4 billion has been agreed.
 * *cost* any costs above £4bn will have to be raised from somewhere;
 * *targetting vs. incentives*: well targetted benefit increases may force poor people into poverty traps, whilst more widely spread increases may require tax increases to keep within budget; 
 * *alternative measures of poverty* have you cheated at all, and reduced poverty headcounts by concentrating on the near-poor, perhaps through increasing in-work benefits? (You may need to do this in order to meet the political objectives)
     """
 end 
 
-# ╔═╡ d447f5dd-253c-4c8a-a2d4-873d50c9a9ec
-begin
-    md"""
-    ##### Taxes and Benefits
-    """
-end 
-
 # ╔═╡ 4aa314f2-3415-4482-a042-d4c7ebd1cb21
 begin
 md"""
-tax allowance: $(@bind tax_allowance NumberField(0:10:25000,default=12570.0)) (p.a.) 
+## Taxes and Benefits
+tax allowance: $(@bind tax_allowance NumberField(0:10:25000,default=12570.0)) (£p.a.; *default £12,570*)
 
-income tax rate: $(@bind income_tax_rate NumberField(0:1:100,default=20) )) (%)
+income tax rate: $(@bind income_tax_rate NumberField(0:1:100,default=20)) (%; *default 20*)
 
-benefit withdrawal rate: $(@bind uc_taper NumberField(0:1:100,default=55) )) (%)
+benefit withdrawal rate: $(@bind uc_taper NumberField(0:1:100,default=55)) (%; *default 55*)
 """
 end
 
@@ -153,7 +147,15 @@ begin
 
 	function fc(v)
 			format( v, precision=0, commas=true )
-	end     
+	end 
+
+	
+	function fb(v)
+			if v ≈ 0
+				return "-"
+			end
+			format( v, precision=0, commas=true )
+	end  
 
 	function make_short_summary( summary :: NamedTuple )::NamedTuple
 		r1 = summary.income_summary[1][1,:]
@@ -212,11 +214,153 @@ Households gaining: **$(res.gainers)** losing: **$(res.losers)** unchanged: **$(
 """
 end
 
+# ╔═╡ a51da585-0668-498e-aa02-ac8d52d44916
+begin
+	function pstate(m, povs )::Int
+		i = 0
+		for p in povs
+			i += 1
+			if m <= p
+				return i
+			end
+		end
+		return i+1
+	end
+
+	function make_povtrans_mat( data :: NamedTuple )::Matrix
+		trans = zeros(6,6)
+		med1 = median(data.indiv[1].eq_bhc_net_income, Weights(data.indiv[1].weight ))
+		povs = med1.*[ 0.3, 0.4, 0.6, 0.8 ]
+		nrows, ncols = size( data.indiv[1])
+		for r in 1:nrows
+			i1 = data.indiv[1][r,:]
+			i2 = data.indiv[2][r,:]
+			p1 = pstate(i1.eq_bhc_net_income, povs)
+			p2 = pstate(i2.eq_bhc_net_income, povs)
+			trans[p1,p2] += i1.weight
+			trans[p1,6]+= i1.weight
+			trans[6,p2]+= i1.weight
+			trans[6,6]+= i1.weight
+		end
+		trans
+	end
+	make_povtrans_mat( data )
+end;
+
+# ╔═╡ 2fe134f3-6d6d-4109-a2f9-faa583be1189
+begin
+
+	
+		s1 = summary.poverty[1]
+		s2 = summary.poverty[2]
+		povrate1 = fp(s1.foster_greer_thorndyke[1])
+		povrate2 = fp(s2.foster_greer_thorndyke[1])
+		dpovrate = fp(s2.foster_greer_thorndyke[1]-s1.foster_greer_thorndyke[1] )
+		povgap1 = fp(s1.foster_greer_thorndyke[3])
+		povgap2 = fp(s2.foster_greer_thorndyke[3])
+		dpovgap = fp(s2.foster_greer_thorndyke[3]-s1.foster_greer_thorndyke[3] )
+		povsev1 = fp(s1.foster_greer_thorndyke[5])
+		povsev2 = fp(s2.foster_greer_thorndyke[5])
+		dpovsev = fp(s2.foster_greer_thorndyke[5]-s1.foster_greer_thorndyke[5] )
+		c1 = summary.child_poverty[1]
+		c2 = summary.child_poverty[2]
+		cp1 = fp(c1.prop)
+	    cp2 = fp(c2.prop)
+		dcp = fp(c2.prop-c1.prop)
+	md"""
+## Poverty Measures
+
+* **Count:** before: **$(povrate1)** after: **$(povrate2)** change: **$(dpovrate)**
+* **Gap:** before: **$(povgap1)** after: **$(povgap2)** change: **$(dpovgap)**
+* **Severity:** before: **$(povsev1)** after: **$(povsev2)** change: **$(dpovsev)**
+* **Child Poverty:** before: **$(cp1)** after: **$(cp2)** change: **$(dcp)**	 
+	""" 
+end
+
+# ╔═╡ aa9d43a0-a45c-48bd-ae28-7b525be605ce
+begin
+		sevcols = [
+			"#ee0000",
+			"#cc2222",
+			"#990000",
+			"#666666",
+			"#333333",
+			"#333333"]
+
+	function one_row( label::String, v :: Vector, r::Int )::String
+		s = "<tr><th style='color:$(sevcols[r])'>$label</th>"
+		
+		for i in 1:5
+			bgcol = if r == 6
+				"#dddddd"
+			elseif i < r
+				"#ffccbb"
+			elseif i > r
+				"#bbffdd"
+			else 
+				"#dddddd"
+			end
+			cell = "<td style='text-align:right;background:$bgcol;color:$(sevcols[i])'>$(v[i])</td>"
+			s *= cell
+		end
+		cell = "<td style='text-align:right;background:#cccccc;color:$(sevcols[r])'>$(v[6])</td>"
+		s *= cell
+		s *= "</tr>"
+		s
+	end
+	
+	function pov_summary( trans::Matrix )::String
+		labels = ["V.Deep (<=30%)",
+		 		 "Deep (<=40%)",
+		 		 "In Poverty (<=60%)",
+		 		 "Near Poverty (<=80%)",
+		 		 "Not in Poverty",
+				 "Total"]
+		vs = fb.(trans)
+		cells = ""
+		for r in 1:6
+			cells *= one_row( labels[r], vs[r,:], r )
+		end
+		
+		 """
+       
+<table width='100%' style=''>
+	<thead></thead>
+	<tr><th colspan='7'>After</th>
+	<tr><th rowspan='9'>Before</th>
+	<tr>
+		<th></th>
+		<th style='color:$(sevcols[1])'>$(labels[1])</th>
+		<th style='color:$(sevcols[2])'>$(labels[2])</th>
+		<th style='color:$(sevcols[3])'>$(labels[3])</th>
+		<th style='color:$(sevcols[4])'>$(labels[4])</th>
+		<th style='color:$(sevcols[5])'>$(labels[5])</th>
+		<th style='color:$(sevcols[6])'>$(labels[6])</th>
+	<tr>
+		$cells
+	</tr>
+</table>
+</td>
+        
+
+        """
+	end
+	trans = make_povtrans_mat( data )
+	t = pov_summary( trans )
+	Show(MIME"text/html"(), t )
+
+# draw_pov_table("run", 90)
+
+end
+
 # ╔═╡ 8cff2a32-35b5-4330-8bfe-0dc604438dba
-hint(md"Maybe try changing the witghdrawal rate.")
+hint(md"Try changing the withdrawal rate. A rate of 100% gives you a MIG, 0 a basic Income.")
 
 # ╔═╡ 67813f80-f4bd-41b3-8963-937e015614c5
 danger(md"Don't forget to commit your saved notebook to your repository.")
+
+# ╔═╡ bada072d-d79b-4bfe-a546-d5df15bf2ea1
+summary
 
 # ╔═╡ Cell order:
 # ╟─3a2a55d5-8cf8-4d5c-80a7-84a03923bba8
@@ -224,8 +368,11 @@ danger(md"Don't forget to commit your saved notebook to your repository.")
 # ╟─5c5b2176-148b-4f5c-a02c-5e9e82df11c3
 # ╟─d069cd4d-7afc-429f-a8fd-3f1c0a640117
 # ╟─d2188dd8-1240-4fdd-870b-dcd15e91f4f2
+# ╟─a51da585-0668-498e-aa02-ac8d52d44916
+# ╟─2fe134f3-6d6d-4109-a2f9-faa583be1189
 # ╟─627959cf-6a7c-4f87-82f7-406f5c7eb76a
-# ╟─d447f5dd-253c-4c8a-a2d4-873d50c9a9ec
+# ╟─aa9d43a0-a45c-48bd-ae28-7b525be605ce
 # ╟─4aa314f2-3415-4482-a042-d4c7ebd1cb21
 # ╟─8cff2a32-35b5-4330-8bfe-0dc604438dba
 # ╟─67813f80-f4bd-41b3-8963-937e015614c5
+# ╟─bada072d-d79b-4bfe-a546-d5df15bf2ea1
