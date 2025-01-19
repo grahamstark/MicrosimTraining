e8f02008cfd6dffd5e65db5af5c8545b9d1d2f82
grahamstark <graham.stark@virtual-worlds.biz>
2025-01-19 23:58:31 +0000

    full proto

--- scripts/init-script.jl
+++ scripts/init-script.jl
@@ -4,7 +4,7 @@ Pkg.gc()
 # STB is unregistered, so ...
 Pkg.add(; url="https://github.com/grahamstark/ScottishTaxBenefitModel.jl") 
 Pkg.instantiate()
-Pkg.update()
-using Revise
+# Pkg.update()
+# using Revise
 using MicrosimTraining
 Pluto.run()
