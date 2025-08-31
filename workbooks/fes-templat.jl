c26000e9d2c993fe80c575fe2824a08bc0640311
Graham Stark <graham.stark@virtual-worlds.biz>
2025-08-31 12:27:45 +0100

    Almost done FES mods.

--- workbooks/fes-templat.jl
+++ workbooks/fes-templat.jl
@@ -4,18 +4,6 @@
 using Markdown
 using InteractiveUtils
 
-# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
-macro bind(def, element)
-    #! format: off
-    return quote
-        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
-        local el = $(esc(element))
-        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
-        el
-    end
-    #! format: on
-end
-
 # ╔═╡ 72c7843c-3698-4045-9c83-2ad391097ad8
 # ╠═╡ show_logs = false
 
@@ -62,31 +50,21 @@ begin
 	sys2 = deepcopy( DEFAULT_SYS)
 	sys2.it.non_savings_rates = sys2.it.non_savings_rates[1:3]
 	sys2.it.non_savings_thresholds = sys2.it.non_savings_thresholds[1:2]
-	sys2.name = "All rates above 21% abolished."
-	sys2
+	sys2.name = "All rates above 21% abolished!"
 end
-
-# ╔═╡ df4ccba6-6e0e-4044-94ce-b89e3c31d7ec
-begin
-# x=reset# react to run being pressed
-# nruns = incr(runs)
-end;
 
 # ╔═╡ a1318fc7-9d20-4c00-8a89-b5ae90b5cc0c
 md"### Poverty Transitions"
 
-# ╔═╡ 7462d54c-aed5-4245-a206-f9f6641148b5
-@bind reset PlutoUI.Button("Reset")
-
 # ╔═╡ 627959cf-6a7c-4f87-82f7-406f5c7eb76a
 # ╠═╡ show_logs = false
-summary, data, short_summary, settings = fes_run( [DEFAULT_SYS, sys2] );
+summary, data, short_summary, timing  = fes_run( [DEFAULT_SYS, sys2] );
 
 # ╔═╡ d069cd4d-7afc-429f-a8fd-3f1c0a640117
 begin
 	
 md"""
-## Summary Results
+## Summary Results : $(sys2.name)
 
 Net Cost of your changes: **$(short_summary.netcost)**
 
@@ -109,7 +87,7 @@ end
 
 # ╔═╡ d2188dd8-1240-4fdd-870b-dcd15e91f4f2
 begin
-	draw_summary_graphs( settings, summary, data )
+	draw_summary_graphs( DEFAULT_FES_SETTINGS, summary, data )
 end
 
 # ╔═╡ 2fe134f3-6d6d-4109-a2f9-faa583be1189
@@ -159,6 +137,16 @@ Show( MIME"text/html"(), format_gainlose("By Tenure",summary.gain_lose[2].ten_gl
 # ╔═╡ 4ed19478-f0bd-4579-87ff-dce95737d60d
 Show( MIME"text/html"(), format_gainlose("By Numbers of Children",summary.gain_lose[2].children_gl ))
 
+# ╔═╡ 6c308ebe-ca45-4774-81cc-bfafc46ba2a4
+begin
+	losers = summary.gain_lose[2].ex_losers
+	hh = FRSHouseholdGetter.get_household( losers[1])
+	l_res1, l_res2 = 
+end
+
+# ╔═╡ c786d3df-8f0e-4eae-a6bc-e5a9d3f4edf2
+gainers = summary.gain_lose[2].ex_gainers
+
 # ╔═╡ bada072d-d79b-4bfe-a546-d5df15bf2ea1
 # summary
 
@@ -166,18 +154,18 @@ Show( MIME"text/html"(), format_gainlose("By Numbers of Children",summary.gain_l
 # ╟─3a2a55d5-8cf8-4d5c-80a7-84a03923bba8
 # ╟─72c7843c-3698-4045-9c83-2ad391097ad8
 # ╟─5c5b2176-148b-4f5c-a02c-5e9e82df11c3
-# ╠═d069cd4d-7afc-429f-a8fd-3f1c0a640117
+# ╟─d069cd4d-7afc-429f-a8fd-3f1c0a640117
 # ╟─0d8df3e0-eeb9-4e61-9298-b735e9dcc284
 # ╠═35e3f85f-581b-45f2-b078-fef31b917f8d
-# ╠═df4ccba6-6e0e-4044-94ce-b89e3c31d7ec
 # ╠═d2188dd8-1240-4fdd-870b-dcd15e91f4f2
-# ╠═2fe134f3-6d6d-4109-a2f9-faa583be1189
+# ╟─2fe134f3-6d6d-4109-a2f9-faa583be1189
 # ╠═a1318fc7-9d20-4c00-8a89-b5ae90b5cc0c
 # ╠═aa9d43a0-a45c-48bd-ae28-7b525be605ce
-# ╟─7462d54c-aed5-4245-a206-f9f6641148b5
 # ╠═627959cf-6a7c-4f87-82f7-406f5c7eb76a
 # ╠═feed5169-225f-4e95-b279-403dff21539d
 # ╟─6bf8bfc0-1221-4055-9c65-ea9b04802321
 # ╟─f1ed5325-1d96-4693-8a2a-64951a04c0ef
 # ╟─4ed19478-f0bd-4579-87ff-dce95737d60d
+# ╠═6c308ebe-ca45-4774-81cc-bfafc46ba2a4
+# ╠═c786d3df-8f0e-4eae-a6bc-e5a9d3f4edf2
 # ╟─bada072d-d79b-4bfe-a546-d5df15bf2ea1
