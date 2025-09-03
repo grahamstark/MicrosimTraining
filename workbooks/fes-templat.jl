8aa436d84d7724919d21f92bc4e1093bc2c0d16c
Graham Stark <graham.stark@virtual-worlds.biz>
2025-09-03 07:00:00 +0100

    Dir for FES.

--- workbooks/fes-templat.jl
+++ workbooks/fes-templat.jl
@@ -1,6 +1,14 @@
 ### A Pluto.jl notebook ###
 # v0.20.17
 
+#> [frontmatter]
+#> language = "en-GB"
+#> title = "FES Project Template"
+#> date = "2025-09-03"
+#> 
+#>     [[frontmatter.author]]
+#>     name = "Graham Stark"
+
 using Markdown
 using InteractiveUtils
 
@@ -13,7 +21,6 @@ import Pkg
 # activate the shared project environment
 Pkg.activate(Base.current_project())
 using MicrosimTraining
-# initialise parameters - 2024 system, annual and pre-weeklyised system
 PlutoUI.TableOfContents(aside=true)
 
 end
@@ -43,7 +50,22 @@ settings = Settings()
 ```
 sets default run settings (number of households to run over, uprating targets, etc.)
 
-The next line:
+
+"""
+
+
+# ╔═╡ 35e3f85f-581b-45f2-b078-fef31b917f8d
+# ╠═╡ show_logs = false
+begin
+	settings = Settings() 
+	wage = 30
+	examples = get_example_hhs(settings)
+	sys1 = deepcopy( DEFAULT_SYS)
+	weeklyise!(sys1)
+end;
+
+# ╔═╡ 2c605323-6b28-4819-9955-1276e4dac14f
+md"""In the next block, the line:
 
 ```julia
 sys2 = deepcopy( DEFAULT_SYS)
@@ -53,40 +75,39 @@ Constructs a parameter system (tax rates, benefit amounts, etc.) which is a copy
 
 ```julia
 it_eq_change = 2.46
-sys2.it.non_savings_rates .+= 2.46
+sys2.it.non_savings_rates .+= it_eq_change
 ```
 
 Adds **2.46%** to each IT band. Everyting else is unchanged. This produces a net revenue gain (after mt benefits) of £2bn. It you don't care about the extra benefits paid, the change would be **£2.42%**.
+
+Only one of the following parameter blocks should be enabled at a time. Disable button is top right in the cell.
+
 """
 
+# ╔═╡ 1237c3e0-949c-40df-91d1-e42092278f99
+md"""
 
-# ╔═╡ 35e3f85f-581b-45f2-b078-fef31b917f8d
-# ╠═╡ show_logs = false
+Likewise the next block adds **9.2%** to the higher rates.
+
+"""
+
+# ╔═╡ 0f97cbf7-1323-4742-9f1c-f1d2dd9ac5e1
+md" Examples of messing with benefits."
+
+# ╔═╡ b40a1662-d43a-48d4-ba09-253b9c34b020
+# ╠═╡ disabled = true
+#=╠═╡
 begin
-	settings = Settings() 
-	it_eq_change = 2.46
-	it_top3_change = 9.2
-	# settings.run_name = "Net £2bn raised with equal increases in all IT rates ($(it_eq_change)pct)."
-	settings.run_name = "Net £2bn raised with higher IT rates ($(it_top3_change)pct)."
-	wage = 30
-	examples = get_example_hhs(settings)
-	sys1 = deepcopy( DEFAULT_SYS)
-	sys2 = deepcopy( DEFAULT_SYS)
-	#=
+	sys2 = deepcopy( DEFAULT_SYS )
 	sys2.it.non_savings_rates = sys2.it.non_savings_rates[1:3]
 	sys2.it.non_savings_thresholds = sys2.it.non_savings_thresholds[1:2]
 	sys2.scottish_child_payment.amount = 0.0
 	sys2.uc.taper = 0
 	sys2.it.personal_allowance = 7_500
 	sys2.nmt_bens.child_benefit.abolished = true
-	=#
-	# it_eq_change = 2.46
-	## eq change sys2.it.non_savings_rates .+= it_eq_change
-	sys2.it.non_savings_rates[4:end] .+= it_top3_change
-	sys2.name = settings.run_name
-	weeklyise!(sys1)
 	weeklyise!(sys2)
-end;
+end
+  ╠═╡ =#
 
 # ╔═╡ 696c6862-1c2b-4d40-a941-44bcbc94e9e2
 md"""
@@ -98,12 +119,6 @@ The next line runs the model every time the block above changes.
 # ╔═╡ 627959cf-6a7c-4f87-82f7-406f5c7eb76a
 # ╠═╡ show_logs = false
 summary, data, short_summary, zipname, timing  = fes_run( settings, [sys1, sys2] );
-
-# ╔═╡ 2534d28e-6d36-4538-a420-88ad7507c4a0
-size(sys2.it.non_savings_rates)
-
-# ╔═╡ fa29ed0c-6373-4512-8538-df3bc47536d3
-zipname, settings.run_name
 
 # ╔═╡ da8d10ef-0ccf-40b9-901c-7214327e0203
 md"""
@@ -133,7 +148,8 @@ md"""
 * Poverty Line :**$(POVERTY_LINE_SOURCE_STRS[settings.poverty_line_source])** $(pov_line_str);
 * Means-Tested Benefits Phase in assumption: **$(MT_ROUTING_STRS[settings.means_tested_routing])**;
 * Disability Benefits Phase in assumption: **Scottish System 100% phased in**;
-* Dodgy Means-Tested Benefits takeup corrections applied: **$(settings.do_dodgy_takeup_corrections)**.
+* Dodgy Means-Tested Benefits takeup corrections applied: **$(settings.do_dodgy_takeup_corrections)**;
+* Output written to directory **$(zipname)**.
 """
 end
 
@@ -308,7 +324,7 @@ Show( MIME"text/html"(), format_gainlose("By Numbers of Children",summary.gain_l
 
 # ╔═╡ 1f7d6f70-0bc3-48ee-ba87-e25f6ba4b907
 begin
-	hh = examples[2]
+	hh = examples[3]
 	bc1, bc2 = getbc( settings, hh.hh, sys1, sys2, wage )
 end;
 
@@ -327,9 +343,6 @@ Show(MIME"text/html"(), format_bc_df( "Pre Budget Constraint $(hh.label)", bc1 )
 
 # ╔═╡ 4718dd2b-9c0f-4c15-b249-52deffee46b6
 Show(MIME"text/html"(), format_bc_df( "Post Budget Constraint  $(hh.label)", bc2 ))
-
-# ╔═╡ 1c9e74b0-f9b4-4979-9793-5e3631927cbc
-
 
 # ╔═╡ 6691e0c2-a440-4f24-855a-6c0c3d746b2e
 md"""## Examples of Gaining Households
@@ -366,16 +379,42 @@ html"""
 </style>
 """
 
+# ╔═╡ 21585926-3451-43e3-a0ad-30860ff37001
+begin
+	sys2 = deepcopy( DEFAULT_SYS )
+	it_top3_change = 9.2
+	settings.run_name = "Net £2bn raised with higher IT rates ($(it_top3_change)pct)."
+	sys2.it.non_savings_rates[4:end] .+= it_top3_change
+	sys2.name = settings.run_name
+	weeklyise!(sys2)
+end
+
+# ╔═╡ 1a1c900a-b65c-4a17-b181-da41883be44f
+# ╠═╡ disabled = true
+#=╠═╡
+begin
+	it_eq_change = 2.46
+	sys2 = deepcopy( DEFAULT_SYS)	
+	settings.run_name = "Net £2bn raised with equal increases in all IT rates ($(it_eq_change)pct)."
+	sys2.it.non_savings_rates .+= it_eq_change
+	weeklyise!(sys2)
+end
+  ╠═╡ =#
+
 # ╔═╡ Cell order:
 # ╟─3a2a55d5-8cf8-4d5c-80a7-84a03923bba8
-# ╟─72c7843c-3698-4045-9c83-2ad391097ad8
+# ╠═72c7843c-3698-4045-9c83-2ad391097ad8
 # ╟─c093e22f-8ec2-4211-b8a0-2391101fbcd2
-# ╟─1f2de37a-948e-4651-9276-eb39743ef812
+# ╠═1f2de37a-948e-4651-9276-eb39743ef812
 # ╠═35e3f85f-581b-45f2-b078-fef31b917f8d
+# ╠═2c605323-6b28-4819-9955-1276e4dac14f
+# ╠═1a1c900a-b65c-4a17-b181-da41883be44f
+# ╟─1237c3e0-949c-40df-91d1-e42092278f99
+# ╠═21585926-3451-43e3-a0ad-30860ff37001
+# ╟─0f97cbf7-1323-4742-9f1c-f1d2dd9ac5e1
+# ╠═b40a1662-d43a-48d4-ba09-253b9c34b020
 # ╟─696c6862-1c2b-4d40-a941-44bcbc94e9e2
 # ╠═627959cf-6a7c-4f87-82f7-406f5c7eb76a
-# ╠═2534d28e-6d36-4538-a420-88ad7507c4a0
-# ╟─fa29ed0c-6373-4512-8538-df3bc47536d3
 # ╟─da8d10ef-0ccf-40b9-901c-7214327e0203
 # ╟─6a57627d-e592-4845-af8a-60d1db327fab
 # ╟─1516e738-7adb-4cb5-9fac-e983ce5d17bd
@@ -400,13 +439,12 @@ html"""
 # ╟─f1ed5325-1d96-4693-8a2a-64951a04c0ef
 # ╟─4ed19478-f0bd-4579-87ff-dce95737d60d
 # ╟─c123f000-bcd6-4a37-b715-759473365b60
-# ╟─1f7d6f70-0bc3-48ee-ba87-e25f6ba4b907
+# ╠═1f7d6f70-0bc3-48ee-ba87-e25f6ba4b907
 # ╠═477c0dc2-9141-49a2-a4c8-fdab84ea586c
 # ╠═8c2c6e7c-53fa-4604-b5dd-85782443ffca
 # ╠═4718dd2b-9c0f-4c15-b249-52deffee46b6
-# ╠═1c9e74b0-f9b4-4979-9793-5e3631927cbc
 # ╟─6691e0c2-a440-4f24-855a-6c0c3d746b2e
 # ╠═6c308ebe-ca45-4774-81cc-bfafc46ba2a4
 # ╠═758496fe-edae-4a3a-9d04-5c09362ec037
 # ╟─34c7ebc0-d137-4572-b68d-3c79d62592d4
-# ╟─1e27cffe-c86c-4b3e-91f4-22e1b429a9cd
+# ╠═1e27cffe-c86c-4b3e-91f4-22e1b429a9cd
